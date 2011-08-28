94e5418f765e5c4ea6c481dade52884c7917be2a
Christian Hill <xn.hill@gmail.com>
2011-08-28 09:26:57 +0100

    Further work on implementing a Django search on the line-by-line database

--- hitranlbl/lbl_searchform.py
+++ hitranlbl/lbl_searchform.py
@@ -1,23 +1,61 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+def convert_to_wavenumber(self, num, units):
+        """
+        Convert the quantity num from units to cm-1. If num is zero and
+        we attempt to divide by it, return None instead.
+
+        """
+
+        if from_units == 'cm-1':
+            return num
+        try:
+            if from_units == 'A':
+                num = 1.e8/num
+            elif from_units == 'nm':
+                num = 1.e7/num
+            elif from_units == 'um':
+                num = 1.e4/num
+            elif from_units == 'Hz':
+                num /= 2.99792458e10
+            elif from_units == 'kHz':
+                num /= 2.99792458e7
+            elif from_units == 'MHz':
+                num /= 2.99792458e4
+            elif from_units == 'GHz':
+                num /= 29.9792458
+            elif from_units == 'THz':
+                num /= 2.99792458e-2
+            else:
+                print 'unrecognised units: %s' % units
+                return None
+        except ZeroDivisionError:
+            return None
+        return num
+
 class LblSearchForm:
     def __init__(self, post_data):
         self.valid = False
+        self.error_msg = '<p class="error_msg">Unspecified error<p>'
         try:
             if post_data.get('numin') == '':
-                self.numin = 0.
+                self.numin = None
             else:
                 self.numin = float(post_data.get('numin'))
             self.numax = float(post_data.get('numax'))
             self.nu_units = post_data.get('numin_units')
-            if post_data.get('Smin') == '':
-                self.Smin = 0.
+
+            self.numin = convert_to_wavenumber(self.numin, self.nu_units)
+            self.numin = convert_to_wavenumber(self.numin, self.nu_units)
+
+            if post_data.get('Swmin') == '':
+                self.Swmin = None
             else:
-                self.Smin = float(post_data.get('Smin'))
-            if self.Smin < 0.:
-                # negative Smin is silly but OK
-                self.Smin = 0.
+                self.Swmin = float(post_data.get('Swmin'))
+            if self.Swmin < 0.:
+                # negative Swmin is silly but OK
+                self.Swmin = None
             self.output_collection = post_data.getlist('output_collection')
             self.datestamp = datetime.strptime(post_data.get('date'),
                     '%Y-%m-%d').date()
@@ -25,26 +63,32 @@ class LblSearchForm:
             return
         if self.numin > self.numax:
             # numin must be <= numax
+            self.error_msg = '<p class="error_msg">Wavenumber/wavelength'
+                'minimum must not be greater than maximum</p>'
             return
 
         selected_molecIDs = post_data.getlist('molecule')
         if not selected_molecIDs:
             # no molecules selected!
+            self.error_msg = '<p class="error_msg">No molecules selected</p>'
             return
         # make sure the selected_molecIDs are integers
         for molecID in selected_molecIDs:
             try:
                 dummy = int(molecID)
             except ValueError:
+                self.error_msg = '<p class="error_msg">Invalid molecule ID'\
+                    ' specified</p>'
                 return
         self.selected_molecIDs = selected_molecIDs
 
+        self.error_msg = ''
         self.valid = True
         return
 
     def is_valid(self):
         if self.valid:
-            return True
-        return False
+            return True, ''
+        return False, self.error_msg
 
 
--- hitranlbl/views.py
+++ hitranlbl/views.py
@@ -2,7 +2,8 @@
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from models import *
-from hitranmeta.models import Molecule, OutputCollection
+from django.db.models import Q
+from hitranmeta.models import Molecule, OutputCollection, Iso
 from lbl_searchform import LblSearchForm
 
 # get a list of molecule objects with entries in the Trans table
@@ -14,19 +15,39 @@ output_collections = OutputCollection.objects.all()
 def index(request):
     if request.POST:
         form = LblSearchForm(request.POST)
-        if form.is_valid():
-            c = {'search_summary': {'summary_html': '<p>Success!</p>'}}
+        form_valid, msg = form.is_valid()
+        if form_valid:
+            search_summary = do_search(form)
+            c = {'search_summary': search_summary}
         else:
-            c = {'search_summary': {'summary_html': '<p>There were errors in your'
-                'search parameters!</p>'}}
+            c = {'search_summary': {'summary_html': '<p>There were errors in'
+                ' your search parameters:</p>%s' % msg}}
         return render_to_response('lbl_searchresults.html', c)
 
     c = {}
     c.update(csrf(request))
     c['present_molecules'] = present_molecules
     c['output_collections'] = output_collections
-    #return render_to_response('index.html', {
-    #            'present_molecules': present_molecules,
-    #            'output_collections': output_collections,
-    #            }) 
     return  render_to_response('index.html', c)
+
+def do_search(form):
+    """
+    Do the search, using the search parameters parsed in the form instance
+    of the LblSearchForm class.
+
+    """
+
+    search_summary = {'summary_html': '<p>Success!</p>'}
+    isos = Iso.objects.filter(molecule__molecID__in=form.selected_molecIDs)
+    # TODO detect when all isotopologues have been selected and ditch this
+    # filter in that case:
+    query = Q(iso__in=isos)
+    if form.numin:
+        query = query & Q(nu__gte=form.numin)
+    if form.numax:
+        query = query & Q(nu__lte=form.numax)
+    if form.Swmin:
+        query = query & Q(Sw__gte=form.Swmin)
+    query = query & Q(valid_from__lte=form.datestamp)
+    query = query & Q(valid_to__gte=form.datestamp)
+    transitions = Trans.objects.filter(query)
