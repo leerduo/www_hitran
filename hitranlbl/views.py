af5d8ba8f010d6152960b4dde129768b27d26029
Christian Hill <xn.hill@gmail.com>
2011-08-28 08:15:33 +0100

    Begin adding search functionality to the django lbl search

--- hitranlbl/views.py
+++ hitranlbl/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from models import *
 from hitranmeta.models import Molecule, OutputCollection
+from lbl_searchform import LblSearchForm
 
 # get a list of molecule objects with entries in the Trans table
 p_ids = Trans.objects.values('iso__molecule').distinct()
@@ -10,7 +12,21 @@ present_molecules = Molecule.objects.filter(molecID__in=p_ids)
 output_collections = OutputCollection.objects.all()
 
 def index(request):
-    return render_to_response('index.html', {
-                'present_molecules': present_molecules,
-                'output_collections': output_collections,
-                }) 
+    if request.POST:
+        form = LblSearchForm(request.POST)
+        if form.is_valid():
+            c = {'search_summary': {'summary_html': '<p>Success!</p>'}}
+        else:
+            c = {'search_summary': {'summary_html': '<p>There were errors in your'
+                'search parameters!</p>'}}
+        return render_to_response('lbl_searchresults.html', c)
+
+    c = {}
+    c.update(csrf(request))
+    c['present_molecules'] = present_molecules
+    c['output_collections'] = output_collections
+    #return render_to_response('index.html', {
+    #            'present_molecules': present_molecules,
+    #            'output_collections': output_collections,
+    #            }) 
+    return  render_to_response('index.html', c)
