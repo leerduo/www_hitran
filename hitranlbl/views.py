62a511c7ec3cf0fc5eda2ba8df0476e3ae5c980e
Christian Hill <xn.hill@gmail.com>
2012-04-11 20:26:04 +0100

    atmos-min complete and reasonably fast

--- hitranlbl/views.py
+++ hitranlbl/views.py
@@ -15,6 +15,12 @@ from itertools import chain
 p_ids = Trans.objects.values('iso__molecule').distinct()
 present_molecules = Molecule.objects.filter(molecID__in=p_ids)
 
+# map globally-unique isotopologue IDs to HITRAN molecID and isoID
+hitranIDs = [(0,0),]    # no iso_id=0
+isos = Iso.objects.all()
+for iso in isos:
+    hitranIDs.append((iso.molecule.molecID, iso.isoID))
+
 output_collections = OutputCollection.objects.all()
 
 def index(request):
@@ -99,52 +105,100 @@ def do_search_atmos_min(form):
 
     start_time = time.time()
 
+    # just the isotopologue ids
+    iso_ids = Iso.objects.filter(molecule__molecID__in=form.selected_molecIDs)\
+                                 .values_list('id', flat=True)
+
+    # NB protect against SQL injection when constructing the query
+    iso_ids_list = ','.join(str(int(id)) for id in iso_ids)
+    q_conds = ['t.iso_id IN (%s)' % iso_ids_list,]
+    if form.numin:
+        q_conds.append('nu>=%f' % form.numin)
+    if form.numax:
+        q_conds.append('nu<=%f' % form.numax)
+    if form.Swmin:
+        q_conds.append('Sw>=%e' % form.Swmin)
+    elif form.Amin:
+        q_conds.append('A>=%e' % form.Amin)
+    q_conds.append('p.name IN ("gamma_air", "gamma_self", "n_air",'\
+                             ' "delta_air")')
+
+    q_fields = ['t.id', 't.iso_id', 't.nu', 't.Sw',
+            't.Elower', 't.gp', 't.statep_id', 't.statepp_id',
+            'GROUP_CONCAT(p.name, "=", p.val)']
+    q_from = 'hitranlbl_trans t INNER JOIN hitranlbl_prm p ON p.trans_id=t.id'
+               
+    q_where = ' AND '.join(q_conds)
+    query = 'SELECT %s FROM %s WHERE %s GROUP BY t.id'\
+                 % (','.join(q_fields), q_from, q_where)
+
     search_summary = {'summary_html':
                 '<p>Here are the results of the query in '\
                 ' atmos-min format</p>'}
 
-    isos = Iso.objects.filter(molecule__molecID__in=form.selected_molecIDs)
-    query = Q(iso__in=isos)
-    if form.numin:
-        query = query & Q(nu__gte=form.numin)
-    if form.numax:
-        query = query & Q(nu__lte=form.numax)
-    if form.Swmin:
-        query = query & Q(Sw__gte=form.Swmin)
-
-    transitions = Trans.objects.filter(query)
-    ntrans = transitions.count()
+    # here's where we do the rawest of the raw SQL query
+    from django.db import connection, transaction
+    cursor = connection.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    ntrans = len(rows)
     te = time.time()
-    print 'time to get transitions = %.1f secs' % (te-start_time)
+    print 'time to get transitions = %.1f secs' % (te - start_time)
 
     ts = time.time()
-    
     filestem = get_filestem()
-    outpath = os.path.join(settings.RESULTSPATH, '%s-trans.txt' % filestem)
-    fo = open(outpath, 'w')
-    for trans in transitions:
-        print >>fo, '%12d%2d%1d%12.6f%10.3e%10.4f%5d%12d%12d%6.4f%6.4f%7.4f'\
-                    '%9.6f'\
-            % (trans.id, trans.iso.molecule.molecID, trans.iso.isoID, trans.nu,
-               trans.Sw, trans.Elower, trans.gp, trans.statep.id,
-               trans.statepp.id,
-               0., 0., 0., 0.) 
-    fo.close()
-
+    output_files = write_atmos_min(filestem, form, rows)
     te = time.time()
-    print 'time to write transitions = %.1f secs' % (te-ts)
+    print 'time to write transitions = %.1f secs' % (te - ts)
 
-    # strip path from output filename:
-    output_files = [os.path.basename(outpath),]
+    # strip path from output filenames:
+    output_files = [os.path.basename(x) for x in output_files]
 
     end_time = time.time()
         
     search_summary['ntrans'] = ntrans
     search_summary['timed_at'] = '%.1f secs' % (end_time - start_time)
 
-    end_time = time.time()
+    return output_files, search_summary 
 
-    return output_files, search_summary
+def write_atmos_min(filestem, form, rows):
+    outpath = os.path.join(settings.RESULTSPATH, '%s-trans.txt' % filestem)
+    fo = open(outpath, 'w')
+
+    if form.default_entry == '-1':
+        default_Epp = '   -1.0000'
+        default_gp = '   -1'
+    else:
+        default_Epp = form.default_entry * 10
+        default_gp = form.default_entry * 5
+
+    for row in rows:
+        try:
+            s_Epp = '%10.4f' % row[4]
+        except TypeError:
+            s_Epp = default_Epp
+        try:
+            s_gp = '%5d' % row[5]
+        except TypeError:
+            s_gp = default_gp
+        prms = row[8].split(',')
+        # NB the defaults for these parameters are 0.
+        s_n_air = ' 0.0000'
+        s_gamma_self = '0.0000'
+        s_delta_air = ' 0.000000'
+        for prm in prms:
+            prm_name, prm_val = prm.split('=')
+            exec('s_%s=prm_val' % prm_name)
+        if len(prms) > 3:
+            s_delta_air = '%s' % prms[3].split('=')[1]
+
+        molecID, isoID = hitranIDs[row[1]]
+        print >>fo, '%12d%2d%1d%12.6f%10.3e%10s%5s%12d%12d %6s %6s %7s %9s'\
+            % (row[0], molecID, isoID,
+               row[2], row[3], s_Epp, s_gp, row[6], row[7],
+               s_gamma_air, s_gamma_self, s_n_air, s_delta_air)
+    fo.close()
+    return [outpath,]
 
 def do_search_astro_min(form):
     """
